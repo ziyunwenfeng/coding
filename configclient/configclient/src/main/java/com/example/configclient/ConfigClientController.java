cc179afe755ed3c3599eff42ff886f9156e0fe22
ziyunwenfeng <1060327272@qq.com>
2018-08-16 14:38:43 +0800

    commit

--- configclient/configclient/src/main/java/com/example/configclient/ConfigClientController.java
+++ configclient/configclient/src/main/java/com/example/configclient/ConfigClientController.java
@@ -3,14 +3,16 @@ package com.example.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 @RefreshScope
 @RestController
-public class ClientController {
-	@Value("${value}")
+public class ConfigClientController {
+	@Value("${from}")
 	String from;
-	@RequestMapping("/from")
-	public String from(){
+	@RequestMapping(value="/from",method=RequestMethod.GET)
+	public String getValue() {
 		return this.from;
 	}
 }
